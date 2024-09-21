fd8ed53d60a9415d90de12f4d2864002e79b7428
NicolasssCR <nicolascardenas311@gmail.com>
2024-09-20 21:49:26 -0500

    Add files via upload

---
+++ Mostrar las tablas HospitalLuzdeVida.sql
@@ -0,0 +1,8 @@
+SHOW TABLES;
+DESCRIBE Agenda;
+DESCRIBE Citas;
+DESCRIBE Consultorios;
+DESCRIBE HorarioCitas;
+DESCRIBE MedicoConsultorio;
+DESCRIBE Medicos;
+Describe Pacientes;
---
+++ Mostrar los registros HospitalLuzdeVida.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Agenda;
+SELECT * FROM Citas;
+SELECT * FROM Consultorios;
+SELECT * FROM HorarioCitas;
+SELECT * FROM MedicoConsultorio;
+SELECT * FROM Medicos;
+SELECT * FROM Pacientes;
