af035c24680269b3c288498953cdec5267c04b7e
NicolasssCR <nicolascardenas311@gmail.com>
2024-10-23 11:32:12 -0500

    Delete Mostrar los registros HospitalLuzdeVida.sql

--- Mostrar los registros HospitalLuzdeVida.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM Agenda;
-SELECT * FROM Citas;
-SELECT * FROM Consultorios;
-SELECT * FROM HorarioCitas;
-SELECT * FROM MedicoConsultorio;
-SELECT * FROM Medicos;
-SELECT * FROM Pacientes;
